2d5c45c2cd22f7b7dbf061733919b447395348dd
Kevin Yeboah <Kevin.Yeboah@caresource.com>
2019-12-04 22:06:49 -0500

    fixed button

--- routes/api/clientHandler.js
+++ routes/api/clientHandler.js
@@ -1,28 +1,48 @@
 function createSearchBox(context) {
+	var currentDiv = document.getElementById("div1");
+
 	var input = document.createElement("input");
     input.type = "text";
     input.id = "input-box"
+	input.style.backgroundColor = "white"
     context.appendChild(input);
+
+//creating the button
+	var button = document.createElement("BUTTON");
+	
+	var text = document.createTextNode("Search");
+	
+	button.appendChild(text);
+	context.appendChild(button);
+	
+
+
 
 
     var h4 = document.createElement("h4")
     h4.id = "search-display"
     context.appendChild(h4)
 	
-	var currentDiv = document.getElementById("div1");
 	
 	var parentDiv = currentDiv.parentNode;
 	
 	parentDiv.insertBefore(input, currentDiv);
+	parentDiv.insertBefore(button,currentDiv);
+
     //var result = document.createTextNode("Search Result: ");
 	//h3.appendChild(result)
 	parentDiv.insertBefore(h4, currentDiv);
 	//document.body.insertBefore(currentDiv, input);
 	//currentDiv.insertBefore(h3, input)
+	
+
+	
 }
+
+
 window.onload = function(context) {
 	//create the input box
-    console.log("WELCOME TO SYNCRAWLER");
+
     createSearchBox(document.body);
 
     //create search content display
@@ -51,8 +71,14 @@ window.onload = function(context) {
 */
     //event listener for when user presses enter
     //when user presses enter, send a request to server with clientid and keyword
-    $(document).on("keypress", "input", function(e){
-        if (e.keyCode == 13) {
+	//$("BUTTON").click(function(e){
+	////e.keyCode == 13;
+
+		//e.keyCode == 13;
+		//};
+	
+    $("BUTTON").on( "click", function(){
+       // if (e.keyCode == 13) {
             console.log("user typed:")
             console.log(document.getElementById('input-box').value);
             
@@ -60,17 +86,25 @@ window.onload = function(context) {
             var xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function() {
                 if (this.readyState == 4 && this.status == 200) {
+					document.getElementById("search-display").innerHTML = ""
                     console.log("the response from the server is:")
                     console.log(this.responseText)
                     document.getElementById("search-display").innerHTML += this.responseText
                 }
             }
 			//location.reload(true);            
-			xhttp.open("POST", `https://syncrawler-server.herokuapp.com/search/clientid/${clientid}`, true);
+			xhttp.open("POST", `http://localhost:3030/search/clientid/${clientid}`, true);
             xhttp.setRequestHeader("Content-Type", "application/json");
             xhttp.send(JSON.stringify({"keyword": document.getElementById('input-box').value}))
-        }
-    });
+        //}
+	});
+	
+	 $(document).on("keypress", "input", function(e){
+        if (e.keyCode == 13) {
+			$("BUTTON").click();
+			}
+	 });
+		//});
 };
 
 
