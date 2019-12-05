e3453da435659770e160d7bfd8c627b61e603366
Kevin Yeboah <Kevin.Yeboah@caresource.com>
2019-12-04 22:38:39 -0500

    helllooo

--- routes/api/clientHandler.js
+++ routes/api/clientHandler.js
@@ -10,7 +10,6 @@ function createSearchBox(context) {
 //creating the button
 	var button = document.createElement("BUTTON");
     button.id = "search-button"
-    button.onclick = search()
 	var text = document.createTextNode("Search");
 	
 	button.appendChild(text);
@@ -55,62 +54,15 @@ window.onload = function(context) {
     script.type = 'text/javascript';
     document.getElementsByTagName('head')[0].appendChild(script);
 
-    //secure clientid of website
-    var clientid = document.getElementById('syncrawler').getAttribute('clientid')
+    document.getElementById("search-button").onclick = search()
     
-/*
-    //send a post request to server of client id or user url to crawl
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            console.log("the server recieved the initial request successfully")
-        }
-    }
-    xhttp.open("POST", `http://localhost:3030/crawl/clientid/${clientid}`, true)
-    //xhttp.setRequestHeader("Content-Type", "application/json")
-    xhttp.send()
-*/
-    //event listener for when user presses enter
-    //when user presses enter, send a request to server with clientid and keyword
-	//$("BUTTON").click(function(e){
-	////e.keyCode == 13;
-
-		//e.keyCode == 13;
-		//};
-    $("#search-button").click(function(){
-       // if (e.keyCode == 13) {
-            console.log("user typed:")
-            console.log(document.getElementById('input-box').value);
-            
-            //XMLHttpRequest object to send request to server
-            var xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
-                if (this.readyState == 4 && this.status == 200) {
-					document.getElementById("search-display").innerHTML = ""
-                    console.log("the response from the server is:")
-                    console.log(this.responseText)
-                    document.getElementById("search-display").innerHTML += this.responseText
-                }
-            }
-			//location.reload(true);            
-			xhttp.open("POST", `https://syncrawler-server.herokuapp.com/search/clientid/${clientid}`, true);
-            xhttp.setRequestHeader("Content-Type", "application/json");
-            xhttp.send(JSON.stringify({"keyword": document.getElementById('input-box').value}))
-        //}
-	});
-	
-	//  $(document).on("keypress", function(e){
-    //     if (e.keyCode == 13) {
-	// 		$("BUTTON").click();
-	// 		}
-	//  });
-};
+}
 
 function search(){
         // if (e.keyCode == 13) {
             console.log("user typed:")
             console.log(document.getElementById('input-box').value);
-            
+            var clientid = document.getElementById('syncrawler').getAttribute('clientid')
             //XMLHttpRequest object to send request to server
             var xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function() {
