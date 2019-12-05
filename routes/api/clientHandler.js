a3a4ecbe6cc1fe50b47a3c64a0dc7d2a79f584bf
Kevin Yeboah <Kevin.Yeboah@caresource.com>
2019-12-04 22:41:17 -0500

    ahhahah

--- routes/api/clientHandler.js
+++ routes/api/clientHandler.js
@@ -54,8 +54,7 @@ window.onload = function(context) {
     script.type = 'text/javascript';
     document.getElementsByTagName('head')[0].appendChild(script);
 
-    document.getElementById("search-button").onclick = search()
-    
+    document.getElementById("search-button").onclick = search
 }
 
 function search(){
